a32135c5665fb7173e63b2aa3321e0c42d09caf7
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-01 20:09:06 +0300

    load iris dataset

--- MlDjangoApp/urls.py
+++ MlDjangoApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.Welcome )
+    # path('admin/', admin.site.urls),
+    path('', views.Welcome, name = 'Welcome' ),
+    path('user', views.User, name = 'User'),
 ]
--- MlDjangoApp/views.py
+++ MlDjangoApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def Welcome(request):
-    return HttpResponse ('Welcome')
+    return render (request, 'index.html')
+
+def User(request):
+    username = request.GET['username']
+    return render(request, 'user.html', {'name': username})
