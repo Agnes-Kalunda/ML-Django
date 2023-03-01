740392f9a1ba66b39a077440c98dbd5e355baa23
Agnes-Kalunda <agypeter97@gmail.com>
2023-03-01 20:31:22 +0300

    added predictor view

--- MlDjangoApp/urls.py
+++ MlDjangoApp/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', views.Welcome, name = 'Welcome' ),
-    path('user', views.User, name = 'User'),
+    path('', views.predictor, name = 'predictor'),
 ]
--- MlDjangoApp/views.py
+++ MlDjangoApp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from joblib import load
+model = load('/home/agnes/ml-django/savedModel/model.joblib')
 
-def Welcome(request):
-    return render (request, 'index.html')
-
-def User(request):
-    username = request.GET['username']
-    return render(request, 'user.html', {'name': username})
+def predictor(request):
+    if request.method == 'POST':
+        sepal_length = request.POST['sepal_length']
+        sepal_width = request.POST['sepal_width']
+        petal_length = request.POST['petal_length']
+        petal_width = request.POST['petal_width']
+        y_pred = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+        if y_pred[0] == 0:
+            y_pred = 'Setosa'
+        elif y_pred[0] == 1:
+            y_pred = 'Verscicolor'
+        else:
+            y_pred = 'Virginica'
+        return render(request, 'main.html', {'result' : y_pred})
+    return render(request, 'main.html')
